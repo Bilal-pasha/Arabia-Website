8d3d3a76bf36ac682eee78d35cefc56dd13c787d
Bilal-Pasha <billopasho56@gmail.com>
2023-11-06 18:20:33 +0500

    slider-autoplay-issue

--- src/Components/BackgroundImage/index.jsx
+++ src/Components/BackgroundImage/index.jsx
@@ -6,16 +6,7 @@ import "./index.css";
 export default function index({ ImageSource, ImageDesciption, style }) {
   return (
     <>
-      <Carousel
-        autoPla={true}
-        infiniteLoop={true}
-        showArrows={true}
-        interval={3000}
-        showIndicators={false}
-        axis={"horizontal"}
-        transitionTime={200}
-        width={"100%"}
-      >
+      <Carousel autoPlay infiniteLoop>
         <div>
           <img src={ImageSource} />
         </div>
