8c0477b08034790682f357cd37382f41473f6d8f
Bilal-Pasha <billopasho56@gmail.com>
2023-11-08 18:20:54 +0500

    about-page-text-animation-issue

--- src/Components/GridTextImage/index.jsx
+++ src/Components/GridTextImage/index.jsx
@@ -1,6 +1,5 @@
-import React, { useRef } from "react";
-import { useInView } from "framer-motion";
-
+import React from "react";
+import { motion } from "framer-motion";
 export default function Grid({
   intro,
   Heading,
@@ -9,20 +8,15 @@ export default function Grid({
   paragraph3,
   ImageSource,
 }) {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
   return (
     <section className="lg:px-32 sm:px-4 bg-white gridbackround">
-      <div className="grid lg:grid-cols-2 sm:grid-rows-1 lg:px-[100px] sm:px-2 lg:py-24 sm:py-12 sm:space-y-8">
-        <div
-          className="col-span-1 lg:space-y-6 sm:space-y-4  "
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-          }}
-          ref={ref}
-        >
+      <motion.div
+        className="grid lg:grid-cols-2 sm:grid-rows-1 lg:px-[100px] sm:px-2 lg:py-24 sm:py-12 sm:space-y-8"
+        initial={{ x: -1000 }}
+        animate={{ x: 0 }}
+        transition={{ duration: "1" }}
+      >
+        <div className="col-span-1 lg:space-y-6 sm:space-y-4  ">
           <p className="lg:text-lg sm:text-sm font-semibold">{intro}</p>
           <h2 className="lg:text-5xl sm:text-3xl font-bold">{Heading}</h2>
           <div className="lg:text-xl sm:text-sm text-[#000000ab] space-y-6">
@@ -31,18 +25,10 @@ export default function Grid({
             <p>{paragraph3}</p>
           </div>
         </div>
-        <div
-          className="col-span-1 justify-self-end"
-          style={{
-            transform: isInView ? "none" : "translateX(-200px)",
-            opacity: isInView ? 1 : 0,
-            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-          }}
-          ref={ref}
-        >
+        <div className="col-span-1 justify-self-end">
           <img src={ImageSource} alt="img" />
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
