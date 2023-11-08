21f0e8900897431c0c9dc98e36dc86eb4de0e6da
Bilal-Pasha <billopasho56@gmail.com>
2023-11-08 17:38:13 +0500

    updated-accordian-button

--- src/Components/Navbar/Accordian/Accordian.jsx
+++ src/Components/Navbar/Accordian/Accordian.jsx
@@ -44,6 +44,14 @@ export default function AccordianComponent() {
             More
           </a>
         </div>
+        <div className="flex flex-col space-y-5 px-2 py-4">
+          <button class="relative px-4 py-2 overflow-hidden border bg-green-900 text-white border-indigo-600 rounded-lg shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-green-600 before:duration-300 before:ease-out hover:text-white hover:shadow-indigo-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80">
+            <span class="relative z-10">Sign in</span>
+          </button>
+          <button class="text-green-900 hover:before:bg-green-600 relative  px-4 py-2 overflow-hidden border  bg-white rounded-lg  shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-green-600 before:transition-all before:duration-50 hover:text-white hover:before:left-0 hover:before:w-full">
+            <span class="relative z-10">Sign up</span>
+          </button>
+        </div>
       </div>
     </>
   );
