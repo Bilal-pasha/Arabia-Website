1232c6c6be867b8014fa1027fbb65aa8fec1da52
Bilal-Pasha <billopasho56@gmail.com>
2023-11-11 11:21:23 +0500

    updated-google-form

--- src/Pages/GoogleForm/index.jsx
+++ src/Pages/GoogleForm/index.jsx
@@ -4,11 +4,11 @@ export default function Form() {
   return (
     <>
       <div className="w-full h-full">
-        <div className="bg-white h-[20vh] w-full flex flex-col justify-center items-center">
-          <h2 className="text-4xl font-bold">
+        <div className="bg-white h-[20vh] w-full flex flex-col justify-center items-center sm:px-5">
+          <h2 className="lg:text-4xl sm:text-2xl font-bold sm:text-center">
             Registration for Inter Madaris Scouts Camping 2023
           </h2>
-          <p className="text-xl pt-3">
+          <p className="lg:text-xl sm:text-sm pt-3">
             Organized by{" "}
             <span class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-yellow-400 relative inline-block">
               <span class="relative text-white">Jamia Arabia Islamia </span>
